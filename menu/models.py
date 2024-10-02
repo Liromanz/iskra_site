f9cfe05d92f7b863aecf96ba99f1934e3fdd8e15
Liromanz <teo.tb@mail.ru>
2024-10-02 12:12:28 +0300

    добавлена гирлянда

--- menu/models.py
+++ menu/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Dish(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название блюда", null=False)
+    price = models.FloatField(verbose_name="Цена в рублях", null=False)
+    additional_info = models.CharField(max_length=20, verbose_name="Количество (гр, шт, мл)", null=True)
+    compound = models.TextField(verbose_name="Состав", null=True)
+    picture = models.ImageField(verbose_name="Фото блюда", null=True, upload_to='menu/img/dishes/')
+
+    class Meta():
+        verbose_name = "Блюдо"
+        verbose_name_plural = "Блюда"
+
