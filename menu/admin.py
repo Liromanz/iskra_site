7480bf85bed3f57cd6aaf612dbca417a865a3a71
Liromanz <teo.tb@mail.ru>
2024-10-03 23:16:05 +0300

    тут типа недописано

--- menu/admin.py
+++ menu/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+admin.site.register(DishCategory)
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'additional_info', 'picture')
+    list_display = ('name', 'category', 'price', 'picture')
     list_display_links = ('name',)
     search_fields = ('name',)
-    list_editable = ('price', 'picture')
+    list_editable = ('category', 'price', 'picture')
+
 
--- menu/models.py
+++ menu/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
+class DishCategory(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название категории", null=False)
+
+    def __str__(self):
+        return f"Категория - {self.name}"
+
+    class Meta():
+        verbose_name = "Категория блюда"
+        verbose_name_plural = "Категории блюд"
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название блюда", null=False)
+    category = models.ForeignKey(DishCategory, models.SET_NULL, null=True, default=None, blank=True)
     price = models.IntegerField(verbose_name="Цена в рублях", null=False)
     additional_info = models.CharField(max_length=20, verbose_name="Количество (гр, шт, мл)", null=True, blank=True )
     compound = models.TextField(verbose_name="Состав", null=True, blank=True)
