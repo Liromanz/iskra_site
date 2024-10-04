de7766be307e414b1abb0604b43c6e0c2dc00b1b
Liromanz <teo.tb@mail.ru>
2024-10-04 04:54:45 +0300

    вербоуз нейм забыла

--- menu/models.py
+++ menu/models.py
@@ -16,7 +16,7 @@ class DishCategory(models.Model):
 
 class Dish(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название блюда", null=False)
-    category = models.ForeignKey(DishCategory, models.SET_NULL, null=True, default=None, blank=True)
+    category = models.ForeignKey( DishCategory, models.SET_NULL, null=True, default=None, blank=True, verbose_name="Категория")
     price = models.IntegerField(verbose_name="Цена в рублях", null=False)
     additional_info = models.CharField(max_length=20, verbose_name="Количество (гр, шт, мл)", null=True, blank=True )
     compound = models.TextField(verbose_name="Состав", null=True, blank=True)
